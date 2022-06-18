339c63c3329f9d8d0149eaf1e263e1c33f53b4c0
2022-VelarOksana <oksana.antonova.77@inbox.ru>
2022-06-19 03:52:47 +0400

    Инициализация массива и разбиение его на подмассивы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Необходимо написать программу, которая из имеющегося массива строк формирует массив из строк,
+// формирует массив из строк, длина которых меньше или равна 3 символам
+string text = "Моя первая контрольная в GeekBrains";
+string[] subs = text.Split(" ");
+foreach (var sub in subs)
+{
+    //Console.WriteLine($"Длина элемента массива {sub} = {sub.Length}");
+    //Console.WriteLine(sub.Length);
+    
+}
+// text = text.Replace ("первая", " ");
+// text = text.Replace ("контрольная", " ");
+// text = text.Replace ("GeekBrains", " ");
+// Console.WriteLine(text);
+
